b98ecd4ee6a2a48ca19743b1fe65b1d2b4639ce8
Nikita <you@example.com>
2023-05-31 17:18:15 +0100

    added words, gitignore, docker

---
+++ words.py
@@ -0,0 +1,16 @@
+"""
+Random words that I use for game
+"""
+movies_words = ['ghostbusters', 'gladiator', 'godfather', 'inception',
+                'interstellar', 'jaws', 'leon', 'matrix',
+                'psycho', 'scream', 'shining', 'terminator',
+                'thor', 'titanic', 'twilight', 'up']
+cars_words = ['audi', 'bmw', 'cadillac', 'chevrolet',
+              'ford', 'honda', 'hyundai', 'jeep',
+              'kia', 'lexus', 'nissan', 'porsche',
+              'subary', 'tesla', 'toyota', 'volkswagen']
+animals_words = ['alligator', 'bear', 'cheetah', 'deer',
+                 'elephant', 'fox', 'giraffe', 'hippo',
+                 'jaguar', 'kangaroo', 'lion', 'monkey',
+                 'otter', 'penguin', 'rabbit', 'shark',
+                 'tiger', 'wolf', 'yak', 'zebra']
